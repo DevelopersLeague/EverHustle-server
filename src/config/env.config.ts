16fa321d777614067d1948e9d9d245f51f2f9635
aniket more <aniketavinashmore343@gmail.com>
2021-03-14 13:57:05 +0530

    add mongo uri

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,24 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.MONGO_USER) {
-  throw new Error(
-    'provide a mongodb username in .env file or environment variable'
-  );
-}
-if (!process.env.MONGO_PASSWORD) {
-  throw new Error(
-    'provide a mongodb password in .env file or environment variable'
-  );
+if (!process.env.MONGO_URI) {
+  throw new Error('provide a mongodb uri in .env file');
 }
 
 export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: Number(process.env.PORT) || 8000,
-
-  MONGO_USER: process.env.MONGO_USER,
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-  MONGO_DATABASE: process.env.MONGO_DATABASE || 'api',
-  MONGO_PORT: process.env.MONGO_PORT || '27017',
-  MONGO_HOST: process.env.MONGO_HOST || 'localhost',
+  MONGO_URI: process.env.MONGO_URI,
 };
--- src/server.ts
+++ src/server.ts
@@ -5,27 +5,19 @@ import mongoose from 'mongoose';
 import { logger } from './common';
 import { env } from './config/env.config';
 
-const myApp = new App([]);
-myApp.expressApp.listen(env.PORT, () => {
-  logger.info(`server started at port ${env.PORT}`);
-});
-
-// mongoose
-//   .connect(
-//     `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}/${env.MONGO_DATABASE}`,
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       authSource: 'admin',
-//     }
-//   )
-//   .then(() => {
-//     logger.info('database connection successful');
-//     const myApp = new App([]);
-//     myApp.expressApp.listen(3000, () => {
-//       logger.info('server started at port 3000');
-//     });
-//   })
-//   .catch((err) => {
-//     logger.error(`connection failed Error:\n ${err.stack}`);
-//   });
+mongoose
+  .connect(env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    authSource: 'admin',
+  })
+  .then(() => {
+    logger.info('database connection successful');
+    const myApp = new App([]);
+    myApp.expressApp.listen(3000, () => {
+      logger.info('server started at port 3000');
+    });
+  })
+  .catch((err) => {
+    logger.error(`connection failed Error:\n ${err.stack}`);
+  });
