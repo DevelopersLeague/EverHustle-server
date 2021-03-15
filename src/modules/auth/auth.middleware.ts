6dbc7f53517fd145026fa60df9f27228ba2fda6c
aniket more <aniketavinashmore343@gmail.com>
2021-03-15 19:41:52 +0530

    add documentation

--- src/modules/auth/auth.middleware.ts
+++ src/modules/auth/auth.middleware.ts
@@ -12,6 +12,10 @@ export class AuthMiddleware {
   constructor(public userService: UserService) {
     this.ensureAuth = this.ensureAuth.bind(this);
   }
+  /**
+   * @description
+   * ensure that the authToken is correct and attach the user object to request object
+   */
   public ensureAuth(req: Request, res: Response, next: NextFunction): void {
     const token = req.headers['authorization']?.split(' ')[1];
     if (token) {
