c65ad699335c6833ba67b2f4865dce6370096fe5
aniket more <aniketavinashmore343@gmail.com>
2021-03-15 12:14:24 +0530

    add check for already existing user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,12 +3,17 @@ import { CreateUserDto } from './dto';
 import bcrypt from 'bcrypt';
 import { IUser, User } from './user.model';
 import { env } from '../../config/env.config';
+import createHttpError from 'http-errors';
 
 @injectable()
 @singleton()
 export class UserService {
   public models = { User: User };
   public async createUser(dto: CreateUserDto): Promise<IUser> {
+    const existingUser = await this.models.User.findOne({ email: dto.email });
+    if (existingUser) {
+      throw new createHttpError.BadRequest('email already taken');
+    }
     const user = new this.models.User();
     user.firstName = dto.firstName;
     user.lastName = dto.lastName;
